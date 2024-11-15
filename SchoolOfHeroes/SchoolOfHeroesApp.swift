6eefc9fb9e12f70bc149a3023c78475a00744886
Andrii Pikus <llityhga@gmail.com>
2024-11-15 10:14:52 +0200

    Initial Commit

---
+++ SchoolOfHeroes/SchoolOfHeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolOfHeroesApp.swift
+//  SchoolOfHeroes
+//
+//  Created by Andrii Pikus on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SchoolOfHeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SchoolOfHeroesTests/SchoolOfHeroesTests.swift
@@ -0,0 +1,16 @@
+//
+//  SchoolOfHeroesTests.swift
+//  SchoolOfHeroesTests
+//
+//  Created by Andrii Pikus on 15.11.2024.
+//
+
+import Testing
+
+struct SchoolOfHeroesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
